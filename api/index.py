bf779071da093ea5984b9995cdc102ccd5049522
LASHMISIMAN.K <lashmisiman206@gmail.com>
2026-09-12 22:35:21 +0530

    error fixed

--- api/index.py
+++ api/index.py
@@ -17,14 +17,14 @@ app.add_middleware(
 )
 
 
-@app.get("/")
+@app.get("/api")
 def home():
     return {
         "message": "Welcome to HireIQ API is running 🚀!"
     }
 
 
-@app.post("/analyze")
+@app.post("/api/analyze")
 async def analyze(
     resume: UploadFile = File(...),
     job_role: str = Form(...)
